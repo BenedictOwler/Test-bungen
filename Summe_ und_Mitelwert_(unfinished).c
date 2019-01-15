7577f6a6f7bd1b770c3ae4708c22b2a58d510226
BenedictOwler <43406176+BenedictOwler@users.noreply.github.com>
2019-01-15 11:46:10 +0100

    Create Summe_ und_Mitelwert_(unfinished).c

---
+++ Summe_ und_Mitelwert_(unfinished).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+void sum(char * Array) {
+
+int x,y,lenght, temp ; 
+
+x = y = temp = 0;
+lenght = strlen(Array);
+
+for (x = "99", y="1");
+
+
+    
+    
+    
+}
+
+
+int main()
+{
+    char Array[] = "99" ;
+    char Array2[] = "1" ;
+    
+printf("Zahlen:\t%s und \t%s", Array, Array2); 
+
+
+
+    return 0;
+}
